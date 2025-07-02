09eaa58ad79e0c057a3d67cf58381be512e758b1
Harvey Marin <hsmc10n@gmail.com>
2025-07-01 21:47:25 -0500

    error, pipes and dtos

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -10,21 +10,29 @@ import {
   HttpStatus,
   HttpCode,
   Res,
+  //ParseIntPipe,
 } from '@nestjs/common';
 
 import { Response } from 'express';
+import { ParseIntPipe } from '../common/parse-int.pipe';
+import { CreateProductDto, UpdateProductDto } from './../dtos/products.dto';
+
+import { ProductsService } from './../services/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
   @Get()
   getProducts(
     @Query('limit') limit: number = 100,
     @Query('offset') offset: number = 0,
     @Query('brand') brand: string,
   ) {
-    return {
-      message: `products limit => ${limit} offset=> ${offset} brand => ${brand}`,
-    };
+    // return {
+    //   message: `products limit => ${limit} offset=> ${offset} brand => ${brand}`,
+    // };
+    return this.productsService.findAll();
   }
 
   @Get('filter')
@@ -34,31 +42,35 @@ export class ProductsController {
 
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
-  getProduct(@Res() response: Response, @Param('productId') productId: string) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  getProduct(@Param('productId', ParseIntPipe) productId: number) {
     //Con Express
-    return response.status(200).send({
-      message: `product ${productId}`,
-    });
+    // return response.status(200).send({
+    //   message: `product ${productId}`,
+    // });
+    return this.productsService.findOne(productId);
   }
 
   @Post()
-  create(@Body() payload: any) {
-    return {
-      message: 'accion de crear',
-      payload,
-    };
+  create(@Body() payload: CreateProductDto) {
+    // return {
+    //   message: 'accion de crear',
+    //   payload,
+    // };
+    return this.productsService.create(payload);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+  update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
+    // return {
+    //   id,
+    //   payload,
+    // };
+    return this.productsService.update(+id, payload);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number) {
-    return id;
+  delete(@Param('id') id: string) {
+    return this.productsService.remove(+id);
   }
 }
