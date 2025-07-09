565564322f984ffa776a1c4b99cac62a735f22a9
Harvey Marin <hsmc10n@gmail.com>
2025-07-09 18:50:08 -0500

    Heroku base config

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
-
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
